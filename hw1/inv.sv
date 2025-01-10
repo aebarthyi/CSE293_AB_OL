7679c8e818343b2cf8deb21c815611fba55592f6
Andrew <abarthyi@ucsc.edu>
2025-01-10 14:08:48 -0800

    starting to create module

---
+++ hw1/inv.sv
@@ -0,0 +1,8 @@
+// This module implements an inverter
+module inv
+  (input [0:0] a_i
+  ,output [0:0] b_o);
+
+   assign b_o = ~a_i;
+
+endmodule
--- hw1/top.sv
+++ hw1/top.sv
@@ -1,1 +1,59 @@
-//TODO: MAKE THE TOP MODULE
+// Top-level design file for the icebreaker FPGA board
+
+/* verilator lint_off PINMISSING */
+/* verilator lint_off UNDRIVEN */
+/* verilator lint_off UNUSEDSIGNAL */
+module top
+  (input [0:0] clk_12mhz_i
+  ,input [0:0] reset_n_async_unsafe_i
+
+  ,output uart_tx_o
+  ,input  uart_tx_i);
+
+   wire clk_o;
+   wire reset_n_sync_r;
+   wire reset_sync_r;
+   
+   wire reset_r; // Use this as your reset_signal
+
+   dff
+     #()
+   sync_a
+     (.clk_i(clk_o)
+     ,.reset_i(1'b0)
+     ,.en_i(1'b1)
+     ,.d_i(reset_n_async_unsafe_i)
+     ,.q_o(reset_n_sync_r));
+
+   inv
+     #()
+   inv
+     (.a_i(reset_n_sync_r)
+     ,.b_o(reset_sync_r));
+
+   dff
+     #()
+   sync_b
+     (.clk_i(clk_o)
+     ,.reset_i(1'b0)
+     ,.en_i(1'b1)
+     ,.d_i(reset_sync_r)
+     ,.q_o(reset_r));
+
+  (* blackbox *)
+  // This is a PLL! You'll learn about these later...
+  SB_PLL40_PAD 
+    #(.FEEDBACK_PATH("SIMPLE")
+     ,.PLLOUT_SELECT("GENCLK")
+     ,.DIVR(4'b0000)
+     ,.DIVF(7'b1000011)
+     ,.DIVQ(3'b101)
+     ,.FILTER_RANGE(3'b001)
+     )
+   pll_inst
+     (.PACKAGEPIN(clk_12mhz_i)
+     ,.PLLOUTCORE(clk_o)
+     ,.RESETB(1'b1)
+     ,.BYPASS(1'b0)
+     );     
+endmodule
